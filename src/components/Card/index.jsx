8c752f4ae3f30c07cc45438e7edaccbb20fd791b
Allyson Rodrigues <125301945+Rodrigues4567@users.noreply.github.com>
2025-05-28 16:48:19 -0300

    FEAT: add the responsive version of section AboutMe (section Technologies and component Card in progress)

---
+++ src/components/Card/index.jsx
@@ -0,0 +1,10 @@
+
+function Card({ icon, name }) {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Card
---
+++ src/sections/Technologies/index.jsx
@@ -0,0 +1,11 @@
+import styles from './Technologies.module.css'
+
+function Technologies() {
+    return (
+        <div className={styles.container}>
+
+        </div>
+    )
+}
+
+export default Technologies
